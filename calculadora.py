e7e06e240d730ed7a5bf6a478ea784e01a84f709
felipeyanci <henriquesfelipesousa@gmail.com>
2025-08-20 21:17:16 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+
+num1 = int(input("primeiro numero:"))
+num2 = int(input("segundo numero:"))
+
+#operacoes
+soma = num1 + num2
+subtracao = num1 - num2 
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+#resultado das operacoes)
+print("subtracao: ", subtracao)
+print("divisao:" , divisao)
+print("multiplicacao:" , multiplicacao)
