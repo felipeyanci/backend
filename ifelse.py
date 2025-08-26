29de02c52d37a6b589b468507f431c93a37b1f32
felipeyanci <henriquesfelipesousa@gmail.com>
2025-08-25 21:13:35 -0300

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite o número da sorte:"))
+
+if numero <=2: 
+    print("Não se preocupe com o futuro, ele já está rindo de você também!")
+elif 2 < numero < 6:
+    print("Hoje ninguém vai te pedir dinheiro imprestado!")
+elif numero == 6: 
+    print("Essa semana você vai ganhar um premio!")
+else:
+    print("Até a próxima!")
